b3f6bdc567fea4d89402764a0e914d9e6a3448ca
Sandro <sandro_malca@usmp.pe>
2023-04-05 12:12:18 -0500

    add sidebar

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import Navbar from "../navbar/Navbar";
-
+import { useSelector } from "react-redux";
+import { Navbar, Sidebar } from "../index";
+import { useGetProducts } from "../../../../services/api/products";
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  //const userId = useSelector((state) => state.global.userId);
+  const data  = useGetProducts();
   return (
-    <Box width="100%" height="100" >
-      <Box>
-        <Navbar />
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={data || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          user={data || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
