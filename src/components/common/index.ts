97a293350f7415093b1cb94dd470ce0199b75eda
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-06 13:39:43 -0300

    loader component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,4 +4,5 @@ export { default as Header } from './layout/header/Header';
 export * from './layout';
 export { default as MenuLink } from './layout/sidemenu/MenuLink/MenuLink';
 export { default as SideMenu } from './layout/sidemenu/SideMenu';
+export { default as Loader } from './loader/Loader';
 export { default as Image } from './react-image/Image';
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,5 @@ export { default as Header } from './common/layout/header/Header';
 export * from './common/layout';
 export { default as MenuLink } from './common/layout/sidemenu/MenuLink/MenuLink';
 export { default as SideMenu } from './common/layout/sidemenu/SideMenu';
+export { default as Loader } from './common/loader/Loader';
 export { default as Image } from './common/react-image/Image';
