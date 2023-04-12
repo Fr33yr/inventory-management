638652564340feb1ecc161b0b2303f2654cc7aec
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-12 08:08:12 -0300

    header icons

--- src/components/common/layout/header/Header.tsx
+++ src/components/common/layout/header/Header.tsx
@@ -1,9 +1,25 @@
-import styles from './Header.module.css'
+import { BellFilled, MailOutlined } from "@ant-design/icons";
+import styles from "./Header.module.css";
+import { Image } from "../../index";
 
 function Header() {
   return (
-    <header className={styles.header}>Header</header>
-  )
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <span>
+          <Image
+            width={50}
+            height={50}
+            url="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          />
+        </span>
+        <div className={styles.navicons}>
+          <BellFilled className={styles.icon}/>
+          <MailOutlined className={styles.icon}/>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
