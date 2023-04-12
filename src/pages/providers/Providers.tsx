7dd9b70a0516d39db9ad906591179125909d8672
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-12 07:26:55 -0300

    Provider component refactor mvvm

--- src/pages/providers/Providers.tsx
+++ src/pages/providers/Providers.tsx
@@ -1,6 +1,6 @@
-import { Avatar, Space, Table, Typography } from "antd";
 import { useEffect, useState } from "react";
 import { getProviders } from "../../../services/api";
+import ProvidersView from './providersview/ProvidersView'
 
 function Providers() {
   const [loading, setLoading] = useState(false);
@@ -15,36 +15,9 @@ function Providers() {
   }, []);
 
   return (
-    <Space size={20} style={{width:1000}} direction="vertical">
-      <Typography.Title level={4}>Customers</Typography.Title>
-      <Table
-        loading={loading}
-        columns={[
-          {
-            title: "Photo",
-            dataIndex: "image",
-            render: (link) => {
-              return <Avatar src={link} />;
-            },
-          },
-          {
-            title: "First Name",
-            dataIndex: "firstName",
-          },
-          {
-            title: "LastName",
-            dataIndex: "lastName",
-          },
-          {
-            title: "Phone",
-            dataIndex: "phone",
-          },
-        ]}
-        dataSource={dataSource}
-        pagination={{
-          pageSize: 5,
-        }}></Table>
-    </Space>
+      <>
+        <ProvidersView loading={loading} dataSource={dataSource}/>
+      </>
   );
 }
 export default Providers;
---
+++ src/pages/providers/providersview/ProvidersView.tsx
@@ -0,0 +1,48 @@
+import { Avatar, Space, Table, Typography } from "antd";
+import styles from "../Providers.module.css";
+
+interface Props {
+  loading: boolean;
+  dataSource: any[];
+}
+
+function ProvidersView({ loading, dataSource }: Props) {
+    
+  return (
+    <div>
+      <Space size={20} className={styles.spaceproviders} direction="vertical">
+        <Typography.Title level={4}>Customers</Typography.Title>
+        <Table
+          loading={loading}
+          columns={[
+            {
+              title: "Photo",
+              dataIndex: "image",
+              render: (link) => {
+                return <Avatar src={link} />;
+              },
+            },
+            {
+              title: "First Name",
+              dataIndex: "firstName",
+            },
+            {
+              title: "LastName",
+              dataIndex: "lastName",
+            },
+            {
+              title: "Phone",
+              dataIndex: "phone",
+            },
+          ]}
+          dataSource={dataSource}
+          pagination={{
+            pageSize: 5,
+          }}
+        ></Table>
+      </Space>
+    </div>
+  );
+}
+
+export default ProvidersView;
