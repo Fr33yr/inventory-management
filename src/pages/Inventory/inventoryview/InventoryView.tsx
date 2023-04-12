5b660466dff390d53ef3ca83fe3d5e398c5904e1
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-12 07:33:57 -0300

    inventory component refactor mvvm

---
+++ src/pages/Inventory/inventoryview/InventoryView.tsx
@@ -0,0 +1,57 @@
+import { Avatar, Rate, Space, Table, Typography } from "antd";
+import styles from '../Inventory.module.css'
+
+interface Props {
+  loading: boolean;
+  dataSource: any[];
+}
+
+function InventoryView({loading, dataSource}:Props) {
+  return (
+    <div className={styles.inventoryview}>
+      <Space size={20} style={{ width: 1000 }} direction="vertical">
+        <Typography.Title level={4}>Inventory</Typography.Title>
+        <Table
+          loading={loading}
+          columns={[
+            {
+              title: "Image",
+              dataIndex: "thumbnail",
+              render: (link) => {
+                return <Avatar src={link} />;
+              },
+            },
+            {
+              title: "Title",
+              dataIndex: "title",
+            },
+            {
+              title: "Price",
+              dataIndex: "price",
+              render: (value) => <span>${value}</span>,
+            },
+            {
+              title: "Stock",
+              dataIndex: "stock",
+            },
+
+            {
+              title: "Brand",
+              dataIndex: "brand",
+            },
+            {
+              title: "Category",
+              dataIndex: "category",
+            },
+          ]}
+          dataSource={dataSource}
+          pagination={{
+            pageSize: 5,
+          }}
+        ></Table>
+      </Space>
+    </div>
+  );
+}
+
+export default InventoryView;
