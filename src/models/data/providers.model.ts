e0fd6b2b1966cffa430334afb6ec624f2bc8e286
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-17 09:57:40 -0300

    providers crud

---
+++ src/models/data/providers.model.ts
@@ -0,0 +1,12 @@
+export interface IProvider {
+  name: string;
+  email: string;
+  phone: string;
+  id: string;
+}
+export interface IProviderFormData {
+  name: string;
+  email: string;
+  phone: string;
+  id: string;
+}
