a9ad109bd8c82a3625e02d4525a621585690fa94
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-18 19:04:21 -0300

    login button useAuth hook implemented

--- src/components/common/buttons/LoginBtn.tsx
+++ src/components/common/buttons/LoginBtn.tsx
@@ -1,12 +1,31 @@
 import { Button } from "antd";
+import { useContext } from "react";
+import { AuthContext, useAuth } from "../../../context/AuthContext";
+
+type Props = {
+  onLogin: Function;
+};
 
 function LoginBtn() {
+  const { user, loginWithGoogle, logout } = useAuth();
 
+  const handleLogin = () => {
+    loginWithGoogle();
+  };
 
+  const handleLoguot = () => {
+    logout();
+  };
 
   return (
     <>
-      <Button title="Login" />
+      <Button
+        title="login"
+        type="primary"
+        onClick={user ? handleLoguot : handleLogin}
+      >
+        {user ? "Logout" : "Login"}
+      </Button>
     </>
   );
 }
