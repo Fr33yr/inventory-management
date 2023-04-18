667cb48a88724654cc9ed882fc69b265665e44ba
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-18 19:04:49 -0300

    authcontext type

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -14,6 +14,7 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../../services/db/config/firebase";
+import { User, UserCredential } from "firebase/auth";
 
 type Children = {
   children: ReactNode;
@@ -24,9 +25,18 @@ export interface loginProps {
   password: string;
 }
 
-type User = {};
+type AuthContextType = {
+  singUp: (email: string, password: string) => Promise<UserCredential>;
+  logIn: (email: string, password: string) => Promise<UserCredential>;
+  loginWithGoogle: () => Promise<UserCredential>;
+  logout: () => Promise<void>;
+  user: User | null;
+  loading: boolean;
+};
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -42,8 +52,10 @@ export function AuthProvider({ children }: Children) {
 
   const singUp = (email: string, password: string) =>
     createUserWithEmailAndPassword(auth, email, password);
+
   const logIn = (email: string, password: string) =>
     signInWithEmailAndPassword(auth, email, password);
+
   const logout = () => signOut(auth);
 
   const loginWithGoogle = () => {
