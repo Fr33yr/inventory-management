0bcb3eaf01907f7b400204348d1d484a9bd01d92
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-18 13:31:18 -0300

    auth config

--- services/db/config/firebase.js
+++ services/db/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export { db };
+const auth = getAuth(app)
+export { db, auth };
