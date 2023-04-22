0f8dad6692419bd5b59f773242de3f6d95821e4d
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-22 13:36:55 -0300

    adapter for orders

---
+++ src/utils/orderAdapter.ts
@@ -0,0 +1,37 @@
+import { IProduct } from "models";
+
+export class OrderAdapter {
+  product: IProduct;
+  constructor(product: IProduct) {
+    this.product = product;
+  }
+
+  getName() {
+    return this.product.name;
+  }
+
+  getBrand() {
+    return this.product.brand;
+  }
+
+  getCategory() {
+    return this.product.category;
+  }
+
+  getAmount() {
+    return 1; // Always set to 1
+  }
+
+  getUnitPrice() {
+    return this.product.price;
+  }
+
+  getSubTotal(){
+    return this.product.price 
+  }
+
+  getProductId() {
+    return this.product.id;
+  }
+  
+}
