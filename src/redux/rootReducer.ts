cf55d6ed8ef1b848499c0d33316a1196ea416b76
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-04-22 13:37:31 -0300

    order reducer and global state

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import productsReducer from "./slices/productsSlices";
 import cartReducer from "./slices/cartSlices";
+import orderReducer from './slices/orderSlice'
 
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
+  orders: orderReducer,
 });
 
 export default rootReducer;
--- src/redux/slices/orderSlice.ts
+++ src/redux/slices/orderSlice.ts
@@ -1,1 +1,92 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IProduct } from "models";
+import { OrderAdapter } from "../../utils/orderAdapter";
+
+interface OrderProduct {
+  name: string;
+  amount: number;
+  unitPrice: number;
+  subTotal: number;
+  productId: string;
+  brand: string;
+  category: string;
+}
+
+interface IOrder {
+  products: OrderProduct[];
+  total: number;
+}
+
+export interface IOrderState {
+  products: OrderProduct[];
+  total: number;
+}
+
+const initialState: IOrderState = {
+  products: [],
+  total: 0,
+};
+
+export const orderSlice = createSlice({
+  name: "orders",
+  initialState,
+  reducers: {
+    addItem: (state, action: PayloadAction<IProduct>) => {
+      const orderAdapter = new OrderAdapter(action.payload);
+      const resultObject: OrderProduct = {
+        name: orderAdapter.getName(),
+        brand: orderAdapter.getBrand(),
+        category: orderAdapter.getCategory(),
+        amount: orderAdapter.getAmount(),
+        unitPrice: orderAdapter.getUnitPrice(),
+        subTotal: orderAdapter.getSubTotal(),
+        productId: orderAdapter.getProductId(),
+      };
+      return {
+        ...state,
+        products: state.products.concat(resultObject),
+      };
+    },
+    setAmount: (state, action) => {
+      let { id, amount } = action.payload;
+      return {
+        ...state,
+        products: state.products.map((obj: OrderProduct) => {
+          if (obj.productId === id) {
+            return { ...obj, amount: amount };
+          } else {
+            return obj;
+          }
+        }),
+      };
+    },
+    removeItem: (state, action) => {
+      let { id } = action.payload;
+      let filteredItems = state.products.filter(
+        (ele: OrderProduct) => ele.productId !== id
+      );
+      return {
+        ...state,
+        products: filteredItems,
+      };
+    },
+    setTotal: (state, action) => {
+      return {
+        ...state,
+        total: action.payload,
+      };
+    },
+    resetOrder: (state, action) => {
+      return {
+        ...state,
+        products: initialState.products,
+        total: initialState.total,
+      };
+    },
+  },
+});
+
+export const { addItem, setAmount, removeItem, setTotal, resetOrder } =
+  orderSlice.actions;
+
+export default orderSlice.reducer;
